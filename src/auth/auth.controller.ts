e78ee9b9758676ce50a6e3f65bd3ad430cec3ff7
FernandoAlfonso <alfonso07amaya@gmail.com>
2025-02-24 12:27:05 -0500

    Configuracion inicial de prisma y docker

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
@@ -8,12 +8,17 @@ import { RegisterDto } from './dto/register.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @Get('watch')
+  Wath() {
+    return this.authService.Wath()
+  }
+
   @Post('/login')
   Login(loginDto: LoginDto) {
     return this.authService.Login(loginDto)
   }
   @Post('/register')
-  Register(registerDto: RegisterDto) {
+  Register(@Body() registerDto: RegisterDto) {
     return this.authService.Register(registerDto)
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AuthService {
+
+  constructor(private readonly prisma: PrismaService) { }
+
+  async Wath() {
+    return await this.prisma.users.findMany()
+  }
 
   Login(data: LoginDto) {
     return data;
   }
 
-  Register(data: RegisterDto) {
-    return data;
+  async Register(data: RegisterDto) {
+    try {
+      const response = await this.prisma.users.create({
+        data
+      });
+      return response
+    } catch (error) {
+      throw new InternalServerErrorException()
+    }
   }
 
 }
